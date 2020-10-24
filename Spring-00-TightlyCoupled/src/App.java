1a3bfa557766573216ac719bdfd9fdbc30a401ff
ugurcancelik <ugurcancelik61@gmail.com>
2020-10-24 18:33:37 -0400

    Tightly Coupled

---
+++ Spring-00-TightlyCoupled/src/App.java
@@ -0,0 +1,12 @@
+public class App {
+
+    public static void main(String[] args) {
+
+        FullTimeMentor fullTime= new FullTimeMentor();
+        PartTimeMentor partTime= new PartTimeMentor();
+
+        Mentor mentor= new Mentor(fullTime,partTime);
+
+        mentor.manageAccount();
+    }
+}
