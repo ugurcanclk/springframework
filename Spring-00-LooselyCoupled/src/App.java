a5b52d961b35c3d17ef8693ecbf01ba757a724f7
ugurcancelik <ugurcancelik61@gmail.com>
2020-10-24 20:02:46 -0400

    Loosely Coupled

---
+++ Spring-00-LooselyCoupled/src/App.java
@@ -0,0 +1,16 @@
+import service.FullTimeMentor;
+import service.MentorAccount;
+import service.PartTimeMentor;
+
+public class App {
+
+    public static void main(String[] args) {
+
+       // FullTimeMentor fullTime= new FullTimeMentor();
+        PartTimeMentor partTime= new PartTimeMentor();
+
+        MentorAccount mentor= new MentorAccount(partTime);
+
+        mentor.manageAccount();
+    }
+}
